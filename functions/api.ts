3c83a54685fb084e9cf5cfc50f7a6011df0eff50
Muhammed <muhammedrefaat2020@gmail.com>
2026-01-23 01:24:11 +0200

    remove netfily

--- functions/api.ts
+++
@@ -1,9 +0,0 @@
-import { Handler } from '@netlify/functions';
-import serverless from 'serverless-http';
-import express from 'express';
-
-import app from '../src/index'; 
-
-const handler: Handler = serverless(app);
-
-export { handler };
