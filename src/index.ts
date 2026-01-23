31ad1c1e30fef40f44befc649cb801d180c8b038
Muhammed <muhammedrefaat2020@gmail.com>
2026-01-23 14:00:34 +0200

    deploy to vercel

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(
-  cors(),
-);
+app.use(cors({ origin: 'http://localhost:3000/' }));
 
 app.use(express.json());
 
